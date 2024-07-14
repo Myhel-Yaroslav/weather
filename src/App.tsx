68b9c293303868c0c398471a15ca86c5589f228a
Myhel-Yaroslav <ist23-ya.myhel@nubip.edu.ua>
2024-07-14 12:37:53 +0300

    App,main

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+
+
+
+const App: FC = () => {
+
+
+	return (
+		<div className='cloud-intro'>
+			<div className='main container'>
+				<div className='glass'>
+					Hello
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
