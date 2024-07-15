52b475ef1a8c0f11758f63c4ef5d114f0503e6a0
Myhel-Yaroslav <ist23-ya.myhel@nubip.edu.ua>
2024-07-15 10:08:54 +0300

    Api,main,app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import './App.scss'
+import { Coord } from './Interfaces/IWeather'
+import CurrentWeather from './components/CurrentWeather/CurrentWeather'
+import Search, { SearchDataType } from './components/Search/Search'
+import WeatherForecast from './components/WeatherForecast/WeatherForecast'
 
-
+export type cityDataType = {
+	city: string
+	coordinates: Coord
+} | null
 
 const App: FC = () => {
+	const [cityData, setCityData] = useState<cityDataType>(null)
 
+	const handleOnSearchChange = (searchData: SearchDataType) => {
+		if (!searchData) return
+		const [lat, lon] = searchData.value.split(' ').map(value => Number(value))
+		const city = searchData.label.toLowerCase()
+		setCityData({ city, coordinates: { lon, lat } })
+	}
 
 	return (
 		<div className='cloud-intro'>
 			<div className='main container'>
 				<div className='glass'>
-					Hello
+					<Search onSearchChange={handleOnSearchChange} />
+					<CurrentWeather cityData={cityData} />
+					<WeatherForecast cityData={cityData} />
 				</div>
 			</div>
 		</div>
